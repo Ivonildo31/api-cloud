415bb9c84c781365ad4679a129f6968b44624900
Álvaro Menezes <alvarollmenezes@users.noreply.github.com>
2016-08-05 16:35:08 -0300

    ...

--- travis.sh
+++ travis.sh
@@ -2,7 +2,7 @@
 
 docker login -e $DOCKER_EMAIL -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker push $DOCKER_IMAGE
-git clone https://github.com/prodest/gerencio-upgrade
+git clone https://github.com/prodest/gerencio-upgrade.git
 cd gerencio-upgrade
 npm install
 node ./gerencio-upgrade $SERVICE_NAME 40000
