b7d60df5555d936ff732de21f451218766c2dc7f
Álvaro Menezes <alvarollmenezes@users.noreply.github.com>
2016-08-05 16:31:15 -0300

    ...

--- travis.sh
+++ travis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSWORD
+docker login -e $DOCKER_EMAIL -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker push $DOCKER_IMAGE
 git clone https://github.com/prodest/gerencio-upgrade
 cd gerencio-upgrade
