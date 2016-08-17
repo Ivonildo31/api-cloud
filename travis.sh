624a2e0764faaa50074d3bdc0896aa0163293c1d
alvarollmenezes <alvarollmenezes@gmail.com>
2016-08-17 19:49:51 -0300

    Corrige o travis

--- travis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker login -e $DOCKER_EMAIL -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push $DOCKER_IMAGE
-git clone https://github.com/prodest/gerencio-upgrade.git
-cd gerencio-upgrade
-npm install
-node ./gerencio-upgrade $SERVICE_NAME 40000
